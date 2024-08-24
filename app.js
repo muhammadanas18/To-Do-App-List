5c65f28d8c593aaca99918933d37f5f6fa22186c
Anas ch <chanas161@gmail.com>
2024-08-24 06:54:26 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let inputBox = document.querySelector('#input-box');
+let listContainer = document.querySelector('#list-container');
+let btn = document.querySelector('button');
+
+btn.addEventListener("click", function() {
+    if(inputBox.value == '') {
+        alert("You must write somenthing!")
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerText = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = "";
+    saveData();
+});
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName == "LI") {
+
+        e.target.classList.toggle("checked");
+        saveData();
+        
+    }
+    else if (e.target.tagName == "SPAN") {
+
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
